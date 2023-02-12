be65be3964c3cfd9f1147751f20ce7c18511fb59
Fauzi <fauzibinfaisal@gmail.com>
2023-02-13 00:09:24 +0700

    delete unused codes

--- rocketapp/Utils/SearchBar.swift
+++ rocketapp/Utils/SearchBar.swift
@@ -32,34 +32,6 @@ struct SearchBar: View {
         .padding(.trailing, 8)
     }
 }
-//struct SearchBar: View {
-//    @Binding var text: String
-//    var body: some View {
-//        HStack {
-//            Image(systemName: "magnifyingglass").foregroundColor(.gray)
-//                .padding(.leading, 8)
-//            TextField("Search rockets", text: $text)
-//                .padding(.horizontal, 8)
-//                .padding(.vertical, 4)
-//                .font(.system(size: 16))
-//
-//            if !text.isEmpty {
-//                Button(action: {
-//                    self.text = ""
-//                }) {
-//                    Image(systemName: "xmark.circle.fill")
-//                        .foregroundColor(.secondary)
-//                }
-//                .padding(.trailing, 8)
-//            }
-//        }
-//        .background(Color(.systemGray6))
-//        .padding(.horizontal, 10)
-//        .padding(.vertical, 8)
-//        .cornerRadius(10)
-//
-//    }
-//}
 
 struct SearchBar_Previews: PreviewProvider {
     @Binding var text: String
